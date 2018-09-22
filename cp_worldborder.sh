59645ea4bb3c2478e9c7d62763a6de6e5f0e5bdc
Falco Prescher <falcoprescher@hotmail.com>
2018-09-22 23:27:35 +0200

    worldboarder copy script

---
+++ cp_worldborder.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker run -v spigot_mc:/mc --name pluginContainer 
+debian:buster /bin/bash
+docker cp ./plugins/WorldBorder.jar pluginContainer:/mc/plugins
+docker stop pluginContainer
+docker rm pluginContainer
