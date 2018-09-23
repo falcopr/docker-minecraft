3c1ff0ff90ce23e23db1108e1a88401915514cf9
Falco Prescher <falcoprescher@hotmail.com>
2018-09-23 11:59:47 +0200

    New basic commands

--- cp_worldborder.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker run -v spigot_mc:/mc --name pluginContainer 
-debian:buster /bin/bash
-docker cp ./plugins/WorldBorder.jar pluginContainer:/mc/plugins
-docker stop pluginContainer
-docker rm pluginContainer
